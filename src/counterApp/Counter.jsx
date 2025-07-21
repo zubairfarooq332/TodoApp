889141fbb2f09609557bcbb9a784c884b78bb493
Zubair  Farooq <zubair.farooq@muztech.io>
2025-07-21 17:07:14 +0500

    complete working counterApp

--- src/counterApp/Counter.jsx
+++ src/counterApp/Counter.jsx
@@ -1,26 +1,42 @@
 import "../counterApp/Counter.css";
+import { useState } from "react";
 import { RiResetLeftFill } from "react-icons/ri";
 import { FaCirclePlus } from "react-icons/fa6";
 import { FaCircleMinus } from "react-icons/fa6";
 
 const Counter = () => {
+  const [value, setvalue] = useState(0);
+  const handleIncrement = () => {
+    setvalue(value + 1);
+  };
+  const handleDecrement = () => {
+    if (value <= 0) {
+      setvalue(0);
+    } else {
+      setvalue(value - 1);
+    }
+  };
+  const resetCounter = () => {
+    setvalue(0);
+  };
+
   return (
     <div className="container">
       <h1>Counter</h1>
       <div className="counter-main">
         <div className="no">
           <div className="display">
-            <p>2</p>
+            <p>{value}</p>
           </div>
-          <div className="reset">
+          <div className="reset" onClick={resetCounter}>
             <RiResetLeftFill />
           </div>
         </div>
         <div className="btns">
-          <button>
+          <button onClick={handleIncrement}>
             <FaCirclePlus />
           </button>
-          <button>
+          <button onClick={handleDecrement}>
             <FaCircleMinus />
           </button>
         </div>
